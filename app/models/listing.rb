a5e26f37ee50ec1d2631814fd62e1e9f65a9aa35
David <idscjun@gmail.com>
2014-10-18 17:28:08 +0900

    move image error

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,6 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x>" }, :default_url => "default.gif"
-        
         validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 	else
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x>" }, :default_url => "default.gif", 
